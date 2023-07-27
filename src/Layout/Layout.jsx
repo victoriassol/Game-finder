636cddd28db3532d32ad21898109123e3d38643f
Victoria <victoriaecalle@gmail.com>
2023-07-27 16:26:42 -0300

    Setup and tailwind

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+
+export default function Layout(){
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js", "./src/App.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
